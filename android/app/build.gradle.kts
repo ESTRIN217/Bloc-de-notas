04d32b7f3e725513a342467013a7b8235fd1cfa2
ESTRIN217 <jhonclenisvelasquezcaraguiche@gmail.com>
2026-04-20 18:25:33 -0400

    material

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -73,7 +73,7 @@ flutter {
 dependencies {
     // ...
     implementation(platform("androidx.compose:compose-bom:2026.03.01"))
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.5.0-alpha17")
     implementation("androidx.activity:activity-compose:1.13.0")
     implementation("androidx.compose.material:material-icons-extended")
 }
